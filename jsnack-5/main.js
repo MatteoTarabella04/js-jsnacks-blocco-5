1e02504daf347958f65297ff5e0a5fa8536d4ba0
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-03-24 12:59:45 +0100

    done

--- jsnack-5/main.js
+++ jsnack-5/main.js
@@ -5,3 +5,17 @@ un array e due numeri ("a" più piccolo di "b" e "b" grande al massimo quanto il
 La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra "a" e "b"
 */
 
+// create array
+const array = [1,2,3,4,5,6,7,8,9,10];
+
+getNumbers(array,1,9);
+
+// funnction to get numbers between a and b 
+function getNumbers(myarray, a, b) {
+   const thisArray = myarray.filter((num, i) => {
+     if (i >= a && i <= b) {
+       return true;
+     }
+   });
+   console.log(thisArray);
+ }
