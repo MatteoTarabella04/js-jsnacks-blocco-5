be346ec91d78aca0cc97b734dd309fcc0d6efdb0
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-03-24 12:16:52 +0100

    initial commit set stages and steps

---
+++ jsnack-1/main.js
@@ -0,0 +1,8 @@
+/* 
+Snack 1:
+Crea un array di 10 oggetti che rappresentano una zucchina, indicando per ognuna varietà, peso e lunghezza. Calcola quanto pesano tutte le zucchine. 
+*/
+
+// create array 'zucchine'
+
+// forEach zucchine get zucchina.peso and do sum of them
